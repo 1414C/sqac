c9f970172b20a04fd2cc79802f48844794242d49
Steve MacLeod <47drln@gmail.com>
2017-10-18 23:09:49 -0600

    spelling

--- sqac_models_test.go
+++ sqac_models_test.go
@@ -25,28 +25,28 @@ type Triplet struct {
 }
 
 type Equipment struct {
-	EquipmentNum int64     `db:"equipment_num" rgen:"primary_key:inc;start:55550000"`
-	ValidFrom    time.Time `db:"valid_from" rgen:"primary_key;nullable:false;default:now()"`
-	ValidTo      time.Time `db:"valid_to" rgen:"primary_key;nullable:false;default:make_timestamptz(9999, 12, 31, 23, 59, 59.9)"`
-	CreatedAt    time.Time `db:"created_at" rgen:"nullable:false;default:now()"`
-	// InspectionAt   time.Time `db:"inspeaction_at" rgen:"nullable:true"`
-	MaterialNum    int     `db:"material_num" rgen:"index:idx_material_num_serial_num"`
-	Description    string  `db:"description" rgen:"rgen:nullable:false"`
-	SerialNum      string  `db:"serial_num" rgen:"index:idx_material_num_serial_num"`
-	IntExample     int     `db:"int_example" rgen:"nullable:false;default:0"`
-	Int64Example   int64   `db:"int64_example" rgen:"nullable:false;default:0"`
-	Int32Example   int32   `db:"int32_example" rgen:"nullable:false;default:0"`
-	Int16Example   int16   `db:"int16_example" rgen:"nullable:false;default:0"`
-	Int8Example    int8    `db:"int8_example" rgen:"nullable:false;default:0"`
-	UIntExample    uint    `db:"u_int_example" rgen:"nullable:false;default:0"`
-	UInt64Example  uint64  `db:"u_int64_example" rgen:"nullable:false;default:0"`
-	UInt32Example  uint32  `db:"u_int32_example" rgen:"nullable:false;default:0"`
-	UInt16Example  uint16  `db:"u_int16_example" rgen:"nullable:false;default:0"`
-	UInt8Example   uint8   `db:"u_int8_example" rgen:"nullable:false;default:0"`
-	Float32Example float32 `db:"float32_example" rgen:"nullable:false;default:0.0"`
-	Float64Example float64 `db:"float64_example" rgen:"nullable:false;default:0.0"`
-	BoolExample    bool    `db:"bool_example" rgen:"nullable:false;default:false"`
-	RuneExample    rune    `db:"rune_example" rgen:"nullable:true"`
-	ByteExample    byte    `db:"byte_example" rgen:"nullable:true"`
+	EquipmentNum   int64     `db:"equipment_num" rgen:"primary_key:inc;start:55550000"`
+	ValidFrom      time.Time `db:"valid_from" rgen:"primary_key;nullable:false;default:now()"`
+	ValidTo        time.Time `db:"valid_to" rgen:"primary_key;nullable:false;default:make_timestamptz(9999, 12, 31, 23, 59, 59.9)"`
+	CreatedAt      time.Time `db:"created_at" rgen:"nullable:false;default:now()"`
+	InspectionAt   time.Time `db:"inspection_at" rgen:"nullable:true"`
+	MaterialNum    int       `db:"material_num" rgen:"index:idx_material_num_serial_num"`
+	Description    string    `db:"description" rgen:"rgen:nullable:false"`
+	SerialNum      string    `db:"serial_num" rgen:"index:idx_material_num_serial_num"`
+	IntExample     int       `db:"int_example" rgen:"nullable:false;default:0"`
+	Int64Example   int64     `db:"int64_example" rgen:"nullable:false;default:0"`
+	Int32Example   int32     `db:"int32_example" rgen:"nullable:false;default:0"`
+	Int16Example   int16     `db:"int16_example" rgen:"nullable:false;default:0"`
+	Int8Example    int8      `db:"int8_example" rgen:"nullable:false;default:0"`
+	UIntExample    uint      `db:"u_int_example" rgen:"nullable:false;default:0"`
+	UInt64Example  uint64    `db:"u_int64_example" rgen:"nullable:false;default:0"`
+	UInt32Example  uint32    `db:"u_int32_example" rgen:"nullable:false;default:0"`
+	UInt16Example  uint16    `db:"u_int16_example" rgen:"nullable:false;default:0"`
+	UInt8Example   uint8     `db:"u_int8_example" rgen:"nullable:false;default:0"`
+	Float32Example float32   `db:"float32_example" rgen:"nullable:false;default:0.0"`
+	Float64Example float64   `db:"float64_example" rgen:"nullable:false;default:0.0"`
+	BoolExample    bool      `db:"bool_example" rgen:"nullable:false;default:false"`
+	RuneExample    rune      `db:"rune_example" rgen:"nullable:true"`
+	ByteExample    byte      `db:"byte_example" rgen:"nullable:true"`
 	Triplet
 }
