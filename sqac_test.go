a92aaacba8a0507b3dcab46894543bdbc1d13d6a
Steve MacLeod <47drln@gmail.com>
2017-11-03 18:07:14 -0600

    CRUD Create in postgres

--- sqac_test.go
+++ sqac_test.go
@@ -932,4 +932,60 @@ func TestNonPersistentColumn(t *testing.T) {
 	if Handle.ExistsColumn(tn, "non_persistent_column") {
 		t.Errorf("column %s should not have been found in table %s", "non_persistent_column", tn)
 	}
+
+	// drop table depot
+	err = Handle.DropTables(Depot{})
+	if err != nil {
+		t.Errorf("failed to drop table %s", tn)
+	}
+}
+
+// TestCRUDCreate
+//
+// Test CRUD Create
+func TestCRUDCreate(t *testing.T) {
+
+	type Depot struct {
+		DepotNum            int       `db:"depot_num" rgen:"primary_key:inc;start:90000000"`
+		CreateDate          time.Time `db:"create_date" rgen:"nullable:false;default:now();index:unique"`
+		Region              string    `db:"region" rgen:"nullable:false;default:YYC"`
+		Province            string    `db:"province" rgen:"nullable:false;default:AB"`
+		Country             string    `db:"country" rgen:"nullable:true;default:CA"`
+		NewColumn1          string    `db:"new_column1" rgen:"nullable:false"`
+		NewColumn2          int64     `db:"new_column2" rgen:"nullable:false"`
+		NewColumn3          float64   `db:"new_column3" rgen:"nullable:false;default:0.0"`
+		NonPersistentColumn string    `db:"non_persistent_column" rgen:"-"`
+	}
+
+	// determine the table names as per the
+	// table creation logic
+	tn := reflect.TypeOf(Depot{}).String()
+	if strings.Contains(tn, ".") {
+		el := strings.Split(tn, ".")
+		tn = strings.ToLower(el[len(el)-1])
+	} else {
+		tn = strings.ToLower(tn)
+	}
+
+	// create table depot
+	err := Handle.CreateTables(Depot{})
+	if err != nil {
+		t.Errorf("%s", err.Error())
+	}
+
+	// expect that table depot exists
+	if !Handle.ExistsTable(tn) {
+		t.Errorf("table %s does not exist", tn)
+	}
+
+	// create a new record via the CRUD Create call
+	var depot = Depot{
+		Region:              "YYC",
+		NewColumn1:          "string_value",
+		NewColumn2:          9999,
+		NewColumn3:          45.33,
+		NonPersistentColumn: "0123456789abcdef",
+	}
+
+	err = Handle.Create(depot)
 }
