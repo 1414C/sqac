d2313a770ce6b903a9fb8d0a794cf1763322ec79
steve macleod <stevem@1414c.io>
2019-11-06 22:42:54 -0600

    correct spelling

--- mssql_flavor.go
+++ mssql_flavor.go
@@ -543,7 +543,7 @@ func (msf *MSSQLFlavor) AlterTables(i ...interface{}) error {
 	}
 	// all table alterations and creations have been completed at this point, with the
 	// exception of the foreign-key creations.  iterate over the fkBuffer, check for
-	// the existance of each foreign-key and create those that do not yet exist.
+	// the existence of each foreign-key and create those that do not yet exist.
 	for _, v := range fkBuffer {
 		fkn, err := common.GetFKeyName(v.ent, v.fkinfo.FromTable, v.fkinfo.RefTable, v.fkinfo.FromField, v.fkinfo.RefField)
 		if err != nil {
